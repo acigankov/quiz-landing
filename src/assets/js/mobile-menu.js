e24c14025978ee25da71043b21eb56322543b72f
acigankov <acigankov@inbox.ru>
2022-11-25 21:30:46 +0300

    шапка

---
+++ src/assets/js/mobile-menu.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.mobile-menu__list');
+
+//отслеживание клика по бургеру
+hamburger.addEventListener('click', function (event) {
+    this.classList.toggle('is-active');
+
+    if(!menu.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+        menu.classList.add('active');
+    } else {
+        document.body.style.overflow = 'auto';
+        menu.classList.remove('active');
+    }
+    
+});
