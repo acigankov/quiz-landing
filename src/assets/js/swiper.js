16ef262a72622381bf1dd57c4dd929049c120c78
acigankov@inbox.ru <diiPs56uaY>
2022-11-27 15:06:07 +0300

    Доделал анимацию слайдеров и большой картники при клике

--- src/assets/js/swiper.js
+++ src/assets/js/swiper.js
@@ -1,9 +1,9 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    // direction: 'vertical',
     loop: true,
-    speed: 800,
+    speed: 300,
     autoplay: true,
+    grabCursor: false,
     //If true, then active slide will be centered, not always on the left side.
     centeredSlides: true,
     centeredSlidesBounds: true,
@@ -17,8 +17,28 @@ const swiper = new Swiper('.swiper', {
     breakpoints: {
         // when window width is >= 992px
         992: {
-          slidesPerView: 3,
+            slidesPerView: 3,
         }
       }
+});
 
-  });
+const fullWrapper = document.querySelector('.image-full__wrapper');
+const close = document.querySelector('.image-full__close svg');
+
+close.addEventListener('click', function (event) {
+        fullWrapper.classList.remove('open');
+});
+
+//собыите самого свайпера по клику
+swiper.on('click', function(e) {
+    if(swiper.clickedSlide.classList.contains('swiper-slide-active')) {
+        const activeSlideImageSrc = document.querySelector('.swiper-slide-active img').getAttribute('src');
+        const fullImageEl = document.querySelector('.image-full__wrapper img');
+        const descriptionText = document.querySelector('.swiper-slide-active .swiper-slide-description__item span').innerHTML;
+        const fullDescriptionEl = document.querySelector('.image-full__wrapper .swiper-slide-description__item span');
+
+        fullWrapper.classList.toggle('open');
+        fullImageEl.setAttribute('src', activeSlideImageSrc);
+        fullDescriptionEl.innerHTML = descriptionText;
+    }
+});
