d70a9f9281ca5dc5897b2cdfe321e02d61081f9e
acigankov <acigankov@inbox.ru>
2022-11-29 23:06:38 +0300

    Сделал секцию механика

---
+++ src/assets/js/mechanic.js
@@ -0,0 +1,7 @@
+const toggler = document.querySelector('.mechanic-text__more');
+const el = document.getElementById('mechanic-list');
+
+toggler.addEventListener('click', function(event) {
+    el.classList.toggle('open');
+    
+});
