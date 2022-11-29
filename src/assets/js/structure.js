2d038741ec94fb7fb24bfe5eba7ed86405fdc95e
acigankov <acigankov@inbox.ru>
2022-11-29 12:43:09 +0300

    Додедал секцию струтура квиза

--- src/assets/js/structure.js
+++ src/assets/js/structure.js
@@ -1,10 +1,39 @@
 const items = document.querySelectorAll('.structure-item');
+const stickers = document.querySelectorAll('.sticker-image');
 
 items.forEach(function(item)  {
-    item.addEventListener('click' , function() {
-        items.forEach(function(item)  {
-            item.classList.remove('active');
-        });
-        this.classList.toggle('active');
+    item.addEventListener('click' , function(event) {
+
+        if(event.target.className != 'sticker-image__image' && !event.target.closest('sticker-image')) {
+            items.forEach(function(item)  {
+                item.classList.remove('active');
+            });
+            this.classList.toggle('active');
+        }
     });
+});
+
+stickers.forEach(function(item)  {
+    item.addEventListener('click', function(event) {
+        stickers.forEach(function(item)  {
+            item.classList.remove('open');
+        });
+
+        this.classList.toggle('open');
+        const title = this.querySelector('.sticker-image__image').dataset.title;
+        const descriptionEl = this.querySelector('.sticker-image__description');
+        
+        descriptionEl.textContent = title;
+        console.log(title);
+        console.log(descriptionEl);
+    });
+});
+
+
+window.addEventListener('click', function(event) {
+    if(event.target.className != 'sticker-image__image') {
+        stickers.forEach(function(item)  {
+            item.classList.remove('open');
+        });
+    }
 });
