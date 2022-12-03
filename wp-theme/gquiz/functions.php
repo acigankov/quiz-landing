f09f339c68ceca8833f4daa6a020ddba8f20b847
acigankov <acigankov@inbox.ru>
2022-12-03 22:31:21 +0300

    Подключение ресурсов в тему

--- wp-theme/gquiz/functions.php
+++ wp-theme/gquiz/functions.php
@@ -139,9 +139,14 @@ add_action( 'widgets_init', 'gquiz_widgets_init' );
  */
 function gquiz_scripts() {
 	wp_enqueue_style( 'gquiz-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'gquiz-main', get_template_directory_uri() . '/assets/css/styles.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'gquiz-bootstrapGrid', get_template_directory_uri() . '/libs/bootstrap-grid.min.css', array(), _S_VERSION );
+	wp_enqueue_style( 'gquiz-swiper', get_template_directory_uri() . '/libs/swiper/swiper-bundle.min.css', array(), _S_VERSION );
 	wp_style_add_data( 'gquiz-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'gquiz-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'gquiz-navigation', get_template_directory_uri() . '/libs/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'gquiz-scripts', get_template_directory_uri() . '/assets/js/custom.min.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'gquiz-swiper', get_template_directory_uri() . '/libs/swiper/swiper-bundle.min.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
